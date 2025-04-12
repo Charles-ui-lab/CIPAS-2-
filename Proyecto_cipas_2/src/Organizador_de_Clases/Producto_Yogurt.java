71e56fee04fe9d5001edf8256ef7484276b4c136
Charles <cstrianac@ut.edu.co>
2025-04-12 12:40:48 -0500

    se añade la clase Producto_Yogurt

--- Proyecto_cipas_2/src/Organizador_de_Clases/Producto_Yogurt.java
+++ Proyecto_cipas_2/src/Organizador_de_Clases/Producto_Yogurt.java
@@ -1,7 +1,8 @@
 package Organizador_de_Clases;
 
-public class Producto_Yogurt extends Productos_Lacteos
-{
+import Organizador_de_Clases.Productos_Lacteos;
+
+public class Producto_Yogurt extends Productos_Lacteos {
     public Producto_Yogurt(String marca, String presentacion, String sabor, double precio) {
         super(marca, presentacion, sabor, precio);
     }
