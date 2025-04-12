a0a5cbe51a9fee60c42c42c75fe436eb52768837
karld <tdel@ut.edu.co>
2025-04-12 11:58:55 -0500

    Agrego Clase Productos lacteos

---
+++ Proyecto_cipas_2/src/Organizador_de_Clases/Productos_Lacteos.java
@@ -0,0 +1,19 @@
+package Organizador_de_Clases;
+
+public class Productos_Lacteos {
+    //Atributos
+    private String marca;
+    private String presentacion;
+    private String sabor;
+    private double precio;
+
+    //Constructor Inicializador de los atributos
+    public Productos_Lacteos(String marca, String presentacion, String sabor, double precio) {
+        this.marca = marca;
+        this.presentacion = presentacion;
+        this.sabor = sabor;
+        this.precio = precio;
+    }
+
+}
+
