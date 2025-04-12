90aded044a115dcd3f91dcb70c9e1907b9b44eba
MAICOL <madiazbar@ut.edu.co>
2025-04-12 12:46:32 -0500

    Agrego el Producto_Kumis

---
+++ Proyecto_cipas_2/src/Organizador_de_Clases/Producto_Kumis.java
@@ -0,0 +1,8 @@
+package Organizador_de_Clases;
+
+public class Producto_Kumis extends Productos_Lacteos{
+
+    public Producto_Kumis(String marca, String presentacion, String sabor, double precio) {
+        super(marca, presentacion, sabor, precio);
+    }
+}
